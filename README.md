ac78e84a1e908c82b2d133b845324cf9fa7faaa6
Marcelo Regys Gomes <marceloregys@gmail.com>
2024-03-02 14:40:22 -0300

    V4.1 - Polimorfismo e Ambientes Virtuais

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # oo-sabor-express
+
+Criando uma venv:
+    python.exe -m venv venv
+
+Ativando uma venv(Windows):
+    .\venv\Scripts\activate.bat
+
+Desativando uma venv(Windows):
+    .\venv\Scripts\deactivate.bat
+
+instalando requests
+    pip install requests
+
+listando pacotes instalados:
+    pip freeze
+
+Criando o requirements.txt
+    pip freeze > requirements.txt
--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from modelos.restaurante import Restaurante #Importa a classe restaurante da pasta modelos
 from modelos.cardapio.bebida import Bebida
 from modelos.cardapio.prato import Prato
+from modelos.cardapio.sobremesa import Sobremesa
 
 restaurante_praca = Restaurante('praça', 'Gourmet') # Cria um restaurante
 #restaurante_mexicano = Restaurante('Mexican Food', 'Mexicana')
 #restaurante_japones = Restaurante('Japa', 'Japonesa')
 
 bebida_suco = Bebida('Suco de Melancia', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho', 2.0, 'O melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
 
 
 '''
@@ -25,8 +30,7 @@ restaurante_praca.alternar_estado()
 
 def main():
     #Restaurante.listar_restaurantes()
-    print(bebida_suco)
-    print(prato_paozinho)
+    restaurante_praca.exibir_cardapio()
 
 if __name__ == '__main__':
     main()
